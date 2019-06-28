8412e011f975b359166eb78868ad7d4ebcdf3348
yijin <yijin@happyshare.com>
2019-06-28 20:30:44 +0800

    init

---
+++ src/main/java/com/example/consulconsumer/ConsulconsumerApplication.java
@@ -0,0 +1,13 @@
+package com.example.consulconsumer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ConsulconsumerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ConsulconsumerApplication.class, args);
+	}
+
+}
